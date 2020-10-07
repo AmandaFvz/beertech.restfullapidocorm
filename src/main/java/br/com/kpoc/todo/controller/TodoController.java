1814317a78d4d360c61efa87b4ede99bb6e079f9
Higor Anjos <higor.araujo.anjos@gmail.com>
2020-10-06 21:43:21 -0300

    Controller Created

--- src/main/java/br/com/kpoc/todo/controller/TodoController.java
+++ src/main/java/br/com/kpoc/todo/controller/TodoController.java
@@ -1,10 +1,13 @@
 package br.com.kpoc.todo.controller;
 
+import br.com.kpoc.todo.controller.dto.TodoDto;
+import br.com.kpoc.todo.model.Todo;
 import br.com.kpoc.todo.service.TodoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/todo")
@@ -16,10 +19,29 @@ public class TodoController {
         this.todoService = todoService;
     }
 
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    @ResponseBody
-//    public void createTodo(@RequestBody TodoDto){
-//
-//    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public void createTodo(@RequestBody TodoDto todoRequest) {
+        Todo todo = new Todo(todoRequest.getTitle(), todoRequest.getDescription());
+        todoService.createTodo(todo);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public void updateTodo(@PathVariable("id") UUID id, @RequestBody TodoDto todoRequest) {
+        Todo todo = new Todo();
+        todo.setId(id);
+        todo.setTitle(todoRequest.getTitle());
+        todo.setDescription(todoRequest.getDescription());
+        todoService.updateTodo(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    public void deleteTodo(@PathVariable("id") UUID id) {
+        todoService.deleteTodo(id);
+    }
 }
